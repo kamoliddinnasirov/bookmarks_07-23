c4e2082404ba22518f46548d63a8a97f7be2510e
ubuntu <kamoliddinnasirov@mail.ru>
2023-12-09 18:59:22 +0500

    ..

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,13 +1,21 @@
 from django.contrib import admin
 from catalog.models import Author, Book, BookInstance, Genre, Language, \
                                 Publisher, Status
+from django.utils.html import format_html
 
+from django.utils.safestring import mark_safe
 
 @admin.register(Author)
 class AuthorAdmin(admin.ModelAdmin):
-    list_display = ('last_name', 'first_name', 'date_of_birth', 'photo')
+    list_display = ('last_name', 'first_name', 'date_of_birth',  "show_photo")
     list_display_links = ('first_name', "last_name")
     fields = ['last_name', 'first_name', ('date_of_birth', 'photo')]
+    readonly_fields = ['show_photo']
+    def show_photo(self, obj):
+        # return format_html(f"<img src='{obj.photo.url}' style='max-height: 100px'>")
+        return mark_safe(f"<img src='{obj.photo.url}' style='max-height: 100px;'>")
+    # show_photo.short_desctiption = "Photo"
+    show_photo.short_desctiption = "Photo"
 
 
 class BooksInstanceInline(admin.TabularInline):
@@ -15,9 +23,13 @@ class BooksInstanceInline(admin.TabularInline):
 
 @admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
-    list_display = ("title", "genre", "language", "display_author")
+    list_display = ("title", "genre", "language", "display_author", "show_photo")
     list_filter = ("genre", "authors")
     inlines = [BooksInstanceInline]
+    readonly_fields = ['show_photo']
+    def show_photo(self, obj):
+        return mark_safe(f"<img src='{obj.photo.url}' style='max-height: 100px;'>")
+    show_photo.short_desctiption = "Photo"
 
 
 
--- catalog/views.py
+++ catalog/views.py
@@ -1,6 +1,27 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from catalog.models import Author, Book, BookInstance, Genre, Language, \
+                                Publisher, Status
 
 
 def index(request):
-    return HttpResponse("Main page")
+    text_head = "Title Bookmarks, ..."
+    books = Book.objects.all()
+    num_books = Book.objects.all().count()
+    num_instances = BookInstance.objects.all().count()
+
+    num_instances_available = BookInstance.objects.filter(status__exact=2).count()
+    authors = Author.objects
+    num_authors = Author.objects.count()
+
+    context = {
+        "text_head" : text_head, 
+        "books" : books,
+        "num_books" : num_books,
+        "num_instances" : num_instances,
+        "num_instances_available" : num_instances_available,
+        "authors" : authors,
+        "num_authors" : num_authors
+    }
+
+    return render(request, 'index.html', context)
